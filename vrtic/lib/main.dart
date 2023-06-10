fa57f87157241be7c45093bd59cb65c6be90994d
skovacic <kowass@gmail.com>
2023-06-10 09:04:34 +0200

    Side drawer

--- vrtic/lib/main.dart
+++ vrtic/lib/main.dart
@@ -9,12 +9,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(title: const Text('Navigation Drawer')),
+        body: const Center(
           child: Text('Hello World!'),
         ),
+        drawer: const NavigationDrawer(),
       ),
     );
+  }
+}
+
+class NavigationDrawer extends StatefulWidget {
+  const NavigationDrawer({super.key});
+
+  @override
+  State<NavigationDrawer> createState() => _NavigationDrawerState();
+}
+
+class _NavigationDrawerState extends State<NavigationDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Text('Da vidimo kak to zgleda'),
+          ),
+          ListTile(
+            title: const Text('Prvi page'),
+            onTap: (){
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Drugi page'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void onPressed() {
   }
 }
