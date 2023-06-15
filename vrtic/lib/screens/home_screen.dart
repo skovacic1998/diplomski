21ea7450038129ff116bea9449be12eb8f369d9f
skovacic1998 <kowass@gmail.com>
2023-06-15 21:54:33 +0200

    Added appbar to homescreen

--- vrtic/lib/screens/home_screen.dart
+++ vrtic/lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vrtic/screens/sign_in_screen.dart';
 
+import '../utils/color_utils.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -13,14 +15,56 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kindergarten Joy'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                hexStringToColor("f4791f"),
+                hexStringToColor("659999"),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Text('Da vidimo kak to zgleda'),
+            ),
+            ListTile(
+              title: const Text('Prvi page'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Drugi page'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           child: const Text("Logout"),
           onPressed: () {
             FirebaseAuth.instance.signOut().then((value) {
               print("Signed out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SignInScreen()));
             });
           },
         ),
