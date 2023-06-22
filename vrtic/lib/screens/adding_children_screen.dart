5a841e2a4a355c22a0a69803ecfda083c9d1aefe
skovacic1998 <kowass@gmail.com>
2023-06-22 17:48:57 +0200

    more ui changes to add children screen

--- vrtic/lib/screens/adding_children_screen.dart
+++ vrtic/lib/screens/adding_children_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../reusable_widgets/reusable_widget.dart';
 import '../utils/color_utils.dart';
 
 class AddChildren extends StatefulWidget {
@@ -12,7 +11,9 @@ class AddChildren extends StatefulWidget {
 
 class _AddChildrenState extends State<AddChildren> {
   TextEditingController nameController = TextEditingController();
-
+  TextEditingController surnameController = TextEditingController();
+  int? selectedOption = 0;
+  List<String> names = ['pero', 'marica', 'barica'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +25,9 @@ class _AddChildrenState extends State<AddChildren> {
             Row(
               children: [
                 const Text('Child name:'),
-                const SizedBox(width: 8,),
+                const SizedBox(
+                  width: 8,
+                ),
                 Expanded(
                   child: SizedBox(
                     height: 50,
@@ -41,14 +44,113 @@ class _AddChildrenState extends State<AddChildren> {
                         fillColor: Colors.white.withOpacity(0.3),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30.0),
-                          borderSide:
-                              const BorderSide(width: 1, style: BorderStyle.none),
+                          borderSide: const BorderSide(
+                              width: 1, style: BorderStyle.none),
                         ),
                       ),
                     ),
                   ),
                 ),
               ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text('Child surname:'),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 50,
+                    child: TextField(
+                      controller: surnameController,
+                      cursorColor: Colors.black,
+                      style: TextStyle(color: Colors.black.withOpacity(0.9)),
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.9),
+                        ),
+                        filled: true,
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        fillColor: Colors.white.withOpacity(0.3),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: const BorderSide(
+                              width: 1, style: BorderStyle.none),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text('Sex: '),
+                const SizedBox(
+                  width: 8,
+                ),
+                Radio(
+                  value: 0,
+                  groupValue: selectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedOption = value;
+                    });
+                  },
+                ),
+                const Text('Male'),
+                const SizedBox(width: 20),
+                Radio(
+                  value: 1,
+                  groupValue: selectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedOption = value;
+                    });
+                  },
+                ),
+                const Text('Female'),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Colors.black26;
+                  }
+                  return Colors.white;
+                }),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                ),
+              ),
+              child: const Text(
+                'Add child',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            SingleChildScrollView(
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: names.length,
+                itemBuilder: (BuildContext ctx, number) {
+                  return Card(
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 100,
+                      child: Center(child: Text(names[number])),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
